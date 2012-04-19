69d8cc9c07dc0ab2797296eb71cf3af1cd9a9cd5
YONG <yong.huang@quantum-info.com>
2012-04-19 16:23:31 +0800

    add 2 method

--- strategy/Duck.java
+++ strategy/Duck.java
@@ -20,4 +20,14 @@ public abstract class Duck
 		System.out.println("All ducks float, even decoys!");
 	}
 	
+	public void setFlyBehavior(FlyBehavior fb)
+	{
+		flyBehavior = fb;
+	}
+	
+	public void setQuackBehavior(QuackBehavior qb)
+	{
+		quackBehavior = qb;
+	}
+	
 }
